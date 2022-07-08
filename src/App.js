c68c761b68d3c2fb9f9edd0b2d80a866e8b9a2b1
vhillyjay <107127737+vhillyjay@users.noreply.github.com>
2022-07-08 11:37:38 +0800

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
   // react logo
 import './App.css';
+import Navbar from './Navbar';
+  // imported from Navbar.js
+import Home from './Home';
+  // imported from Home.js
 
 function App() {
-  const title = "Welcome to the new Blog";
-  const likes = 55;
-  const link = "http://www.google.com";
+  // const title = "Welcome to the new Blog";
+  // const likes = 55;
+  // const link = "http://www.google.com";
     // string number array allowed
     // objects not allowed
 
   return (
     <div className="App">
+      <Navbar />
+        {/* from import Navbar at the top */}
       <div className='content'>
-        <h1> {title} </h1>
+        <Home />
+        {/* <h1> {title} </h1>
         <p>Liked  {likes} times</p>
-        <a href={link}>Google</a>
+        <a href={link}>Google</a> */}
       </div>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
